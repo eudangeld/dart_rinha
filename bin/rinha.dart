25a7748d5fbc65d8605fc73d44f4a1dd25417333
dannylodangel <130377552+dannylodangel@users.noreply.github.com>
2023-09-05 20:30:42 -0300

    starting

---
+++ bin/rinha.dart
@@ -0,0 +1,5 @@
+import 'package:rinha/rinha.dart' as rinha;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rinha.calculate()}!');
+}
---
+++ test/rinha_test.dart
@@ -0,0 +1,8 @@
+import 'package:rinha/rinha.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
